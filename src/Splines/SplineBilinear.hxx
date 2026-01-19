11e4f5f28a9ae1f72822a8ea8d8d790633f00f53
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2026-01-19 21:05:28 +0100

    refactored bilinear

--- src/Splines/SplineBilinear.hxx
+++ src/Splines/SplineBilinear.hxx
@@ -17,33 +17,37 @@
  |                                                                          |
 \*--------------------------------------------------------------------------*/
 
-/*\
- |   ____  _ _ _                       ____        _ _
- |  | __ )(_) (_)_ __   ___  __ _ _ __/ ___| _ __ | (_)_ __   ___
- |  |  _ \| | | | '_ \ / _ \/ _` | '__\___ \| '_ \| | | '_ \ / _ \
- |  | |_) | | | | | | |  __/ (_| | |   ___) | |_) | | | | | |  __/
- |  |____/|_|_|_|_| |_|\___|\__,_|_|  |____/| .__/|_|_|_| |_|\___|
- |                                          |_|
-\*/
+#pragma once
+
+#ifndef SPLINE_BILINEAR_HXX
+#define SPLINE_BILINEAR_HXX
 
 namespace Splines
 {
 
+  /*\
+   |   ____  _ _ _                       ____        _ _
+   |  | __ )(_) (_)_ __   ___  __ _ _ __/ ___| _ __ | (_)_ __   ___
+   |  |  _ \| | | | '_ \ / _ \/ _` | '__\___ \| '_ \| | | '_ \ / _ \
+   |  | |_) | | | | | | |  __/ (_| | |   ___) | |_) | | | | | |  __/
+   |  |____/|_|_|_|_| |_|\___|\__,_|_|  |____/| .__/|_|_|_| |_|\___|
+   |                                          |_|
+  \*/
+
   //! bilinear spline base class
   class BilinearSpline : public SplineSurf
   {
+    using SplineSurf::m_nx;
+    using SplineSurf::m_ny;
+    using SplineSurf::m_X;
+    using SplineSurf::m_Y;
+    using SplineSurf::m_Z;
+
     void make_spline() override
     {
       m_search_x.must_reset();
       m_search_y.must_reset();
     }
-
-    using SplineSurf::m_nx;
-    using SplineSurf::m_ny;
-
-    using SplineSurf::m_X;
-    using SplineSurf::m_Y;
-    using SplineSurf::m_Z;
 
   public:
     using SplineSurf::eval;
@@ -60,13 +64,114 @@ namespace Splines
     //!
     ~BilinearSpline() override {}
 
-    real_type eval( real_type const x, real_type const y ) const override;
+    //! Evaluate the spline at (x,y)
+    real_type eval( real_type const x, real_type const y ) const override
+    {
+      std::pair<integer, real_type> X( 0, x ), Y( 0, y );
 
-    void      D( real_type const x, real_type const y, real_type d[3] ) const override;
-    real_type Dx( real_type const x, real_type const y ) const override;
-    real_type Dy( real_type const x, real_type const y ) const override;
+      m_search_x.find( X );
+      m_search_y.find( Y );
 
-    void      DD( real_type const x, real_type const y, real_type dd[6] ) const override;
+      integer const   i{ X.first };
+      integer const   j{ Y.first };
+      real_type const DX{ m_X[i + 1] - m_X[i] };
+      real_type const DY{ m_Y[j + 1] - m_Y[j] };
+      real_type const u{ ( X.second - m_X[i] ) / DX };
+      real_type const v{ ( Y.second - m_Y[j] ) / DY };
+      real_type const u1{ 1 - u };
+      real_type const v1{ 1 - v };
+
+      real_type const Z00{ m_Z[ipos_C( i, j )] };
+      real_type const Z01{ m_Z[ipos_C( i, j + 1 )] };
+      real_type const Z10{ m_Z[ipos_C( i + 1, j )] };
+      real_type const Z11{ m_Z[ipos_C( i + 1, j + 1 )] };
+
+      return u1 * ( Z00 * v1 + Z01 * v ) + u * ( Z10 * v1 + Z11 * v );
+    }
+
+    //! Compute x-derivative at (x,y)
+    real_type Dx( real_type const x, real_type const y ) const override
+    {
+      std::pair<integer, real_type> X( 0, x ), Y( 0, y );
+
+      m_search_x.find( X );
+      m_search_y.find( Y );
+
+      integer const i{ X.first };
+      integer const j{ Y.first };
+
+      real_type const DX{ m_X[i + 1] - m_X[i] };
+      real_type const DY{ m_Y[j + 1] - m_Y[j] };
+      real_type const v{ ( Y.second - m_Y[j] ) / DY };
+
+      real_type const Z00{ m_Z[ipos_C( i, j )] };
+      real_type const Z01{ m_Z[ipos_C( i, j + 1 )] };
+      real_type const Z10{ m_Z[ipos_C( i + 1, j )] };
+      real_type const Z11{ m_Z[ipos_C( i + 1, j + 1 )] };
+      return ( ( Z10 - Z00 ) * ( 1 - v ) + ( Z11 - Z01 ) * v ) / DX;
+    }
+
+    //! Compute y-derivative at (x,y)
+    real_type Dy( real_type const x, real_type const y ) const override
+    {
+      std::pair<integer, real_type> X( 0, x ), Y( 0, y );
+
+      m_search_x.find( X );
+      m_search_y.find( Y );
+
+      integer const i{ X.first };
+      integer const j{ Y.first };
+
+      real_type const DX{ m_X[i + 1] - m_X[i] };
+      real_type const DY{ m_Y[j + 1] - m_Y[j] };
+      real_type const u{ ( X.second - m_X[i] ) / DX };
+
+      real_type const Z00{ m_Z[ipos_C( i, j )] };
+      real_type const Z01{ m_Z[ipos_C( i, j + 1 )] };
+      real_type const Z10{ m_Z[ipos_C( i + 1, j )] };
+      real_type const Z11{ m_Z[ipos_C( i + 1, j + 1 )] };
+      return ( ( Z01 - Z00 ) * ( 1 - u ) + ( Z11 - Z10 ) * u ) / DY;
+    }
+
+    //! Compute value and first derivatives at (x,y)
+    void D( real_type const x, real_type const y, real_type d[3] ) const override
+    {
+      std::pair<integer, real_type> X( 0, x ), Y( 0, y );
+
+      m_search_x.find( X );
+      m_search_y.find( Y );
+
+      integer const i{ X.first };
+      integer const j{ Y.first };
+
+      real_type const DX{ m_X[i + 1] - m_X[i] };
+      real_type const DY{ m_Y[j + 1] - m_Y[j] };
+
+      real_type const u{ ( X.second - m_X[i] ) / DX };
+      real_type const v{ ( Y.second - m_Y[j] ) / DY };
+      real_type const u1{ 1 - u };
+      real_type const v1{ 1 - v };
+
+      real_type const Z00{ m_Z[this->ipos_C( i, j )] };
+      real_type const Z01{ m_Z[this->ipos_C( i, j + 1 )] };
+      real_type const Z10{ m_Z[this->ipos_C( i + 1, j )] };
+      real_type const Z11{ m_Z[this->ipos_C( i + 1, j + 1 )] };
+
+      d[0] = u1 * ( Z00 * v1 + Z01 * v ) + u * ( Z10 * v1 + Z11 * v );
+      d[1] = v1 * ( Z10 - Z00 ) + v * ( Z11 - Z01 );
+      d[1] /= DX;
+      d[2] = u1 * ( Z01 - Z00 ) + u * ( Z11 - Z10 );
+      d[2] /= DY;
+    }
+
+    //! Compute value and all derivatives up to second order at (x,y)
+    void DD( real_type const x, real_type const y, real_type dd[6] ) const override
+    {
+      this->D( x, y, dd );
+      dd[3] = dd[4] = dd[5] = 0;  // second derivative are 0
+    }
+
+    //! Second derivatives are always zero for bilinear splines
     real_type Dxx( real_type const, real_type const ) const override { return 0; }
     real_type Dxy( real_type const, real_type const ) const override { return 0; }
     real_type Dyy( real_type const, real_type const ) const override { return 0; }
@@ -76,12 +181,36 @@ namespace Splines
     //! \name Autodiff
     //!
     ///@{
-    autodiff::dual1st eval( autodiff::dual1st const & x, autodiff::dual1st const & y ) const;
-    autodiff::dual2nd eval( autodiff::dual2nd const & x, autodiff::dual2nd const & y ) const;
+    autodiff::dual1st eval( autodiff::dual1st const & x, autodiff::dual1st const & y ) const
+    {
+      using autodiff::dual1st;
+      using autodiff::detail::val;
+
+      real_type dd[3];
+      D( val( x ), val( y ), dd );
+
+      dual1st res{ dd[0] };
+      res.grad = dd[1] * x.grad + dd[2] * y.grad;
+
+      return res;
+    }
+
+    autodiff::dual2nd eval( autodiff::dual2nd const & x, autodiff::dual2nd const & y ) const
+    {
+      using autodiff::derivative;
+      using autodiff::dual2nd;
+
+      real_type dd[6];
+      DD( val( x ), val( y ), dd );
+
+      dual2nd res{ dd[0] };
+      res.grad = dd[1] * x.grad + dd[2] * y.grad;
+      // res.grad.grad = dd[3]*dx*dx + 2*dx*dy*dd[4]+ dy*dy*dd[5] + ddx*dd[1] + ddy*dd[2]; always 0
+      return res;
+    }
 
     template <typename T1, typename T2>
-    autodiff::HigherOrderDual<autodiff::detail::DualOrder<T1, T2>::value, real_type> eval( T1 const & x, T2 const & y )
-      const
+    autodiff::HigherOrderDual<autodiff::detail::DualOrder<T1, T2>::value, real_type> eval( T1 const & x, T2 const & y ) const
     {
       autodiff::HigherOrderDual<autodiff::detail::DualOrder<T1, T2>::value, real_type> X{ x }, Y{ y };
       return eval( X, Y );
@@ -89,10 +218,43 @@ namespace Splines
 ///@}
 #endif
 
-    void         write_to_stream( ostream_type & s ) const override;
-    char const * type_name() const override;
+    //! Write spline information to output stream
+    void write_to_stream( ostream_type & s ) const override
+    {
+      fmt::print( s, "Nx={} Ny={}\n", m_nx, m_ny );
+      for ( integer i = 1; i < m_nx; ++i )
+      {
+        for ( integer j = 1; j < m_ny; ++j )
+        {
+          integer const i00{ ipos_C( i - 1, j - 1 ) };
+          integer const i10{ ipos_C( i, j - 1 ) };
+          integer const i01{ ipos_C( i - 1, j ) };
+          integer const i11{ ipos_C( i, j ) };
+          fmt::print(
+            s,
+            "patch ({},{})\n"
+            "  DX    = {:<12.4}  DY    = {:<12.4}\n"
+            "  Z00   = {:<12.4}  Z10   = {:<12.4}\n"
+            "  Z01   = {:<12.4}  Z11   = {:<12.4}\n",
+            i,
+            j,
+            m_X[i] - m_X[i - 1],
+            m_Y[j] - m_Y[j - 1],
+            m_Z[i00],
+            m_Z[i10],
+            m_Z[i01],
+            m_Z[i11] );
+        }
+      }
+    }
+
+    //! Return spline type name
+    char const * type_name() const override
+    {
+      return "bilinear";
+    }
   };
 
 }  // namespace Splines
 
-// EOF: SplineBilinear.hxx
+#endif  // SPLINE_BILINEAR_HXX
--- src/Splines/SplineHermite.hxx
+++ src/Splines/SplineHermite.hxx
@@ -17,17 +17,255 @@
  |                                                                          |
 \*--------------------------------------------------------------------------*/
 
-/*\
- |    _   _                     _ _       ____        _ _
- |   | | | | ___ _ __ _ __ ___ (_) |_ ___/ ___| _ __ | (_)_ __   ___
- |   | |_| |/ _ \ '__| '_ ` _ \| | __/ _ \___ \| '_ \| | | '_ \ / _ \
- |   |  _  |  __/ |  | | | | | | | ||  __/___) | |_) | | | | | |  __/
- |   |_| |_|\___|_|  |_| |_| |_|_|\__\___|____/| .__/|_|_|_| |_|\___|
- |                                             |_|
-\*/
+#ifndef SPLINE_HERMITE_HXX
+#define SPLINE_HERMITE_HXX
 
 namespace Splines
 {
+
+  /*\
+   |   _   _                     _ _
+   |  | | | | ___ _ __ _ __ ___ (_) |_ ___
+   |  | |_| |/ _ \ '__| '_ ` _ \| | __/ _ \
+   |  |  _  |  __/ |  | | | | | | | ||  __/
+   |  |_| |_|\___|_|  |_| |_| |_|_|\__\___|
+  \*/
+
+  // --------------------------------------------------------------------------
+
+inline void Hermite5(real_type x, real_type H, real_type base[6])
+{
+  // Potenze di base
+  const real_type x2 = x * x;
+  const real_type x3 = x2 * x;
+  const real_type x4 = x2 * x2;
+
+  const real_type H2 = H * H;
+  const real_type H4 = H2 * H2;
+
+  // (H - x)
+  const real_type dx  = H - x;
+  const real_type dx2 = dx * dx;
+  const real_type dx3 = dx2 * dx;
+
+  // Inversi
+  const real_type invH  = real_type(1) / H;
+  const real_type invH4 = real_type(1) / H4;
+  const real_type invH5 = invH * invH4;
+
+  // Base functions
+  base[0] = invH5 * dx3 * (3 * x * H + H2 + 6 * x2);
+
+  base[1] = invH5 * (
+              -15 * H * x4 +
+                6 * x4 * x +
+               10 * H2 * x3
+            );
+
+  base[2] = invH4 * dx3 * x * (H + 3 * x);
+
+  base[3] = invH4 * (3 * x - 4 * H) * dx * x3;
+
+  const real_type c = invH / (2 * H2);
+
+  base[4] = c * dx3 * x2;
+  base[5] = c * dx2 * x3;
+}
+
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+inline void Hermite5_D(real_type x, real_type H, real_type base_D[6])
+{
+  // Differenza
+  const real_type dx  = H - x;
+  const real_type dx2 = dx * dx;
+
+  // Potenze di x
+  const real_type x2 = x * x;
+  const real_type x4 = x2 * x2;
+
+  // Potenze di H
+  const real_type H2 = H * H;
+  const real_type H4 = H2 * H2;
+
+  // Inversi
+  const real_type invH  = real_type(1) / H;
+  const real_type invH4 = real_type(1) / H4;
+
+  // Termine comune
+  const real_type common = 30 * x2 * dx2 * invH * invH4;
+
+  // Derivate delle basi
+  base_D[0] = -common;
+  base_D[1] =  common;
+
+  base_D[2] = invH4 * (H - 3 * x) * (H + 5 * x) * dx2;
+
+  base_D[3] = invH4 * (
+                x2 * (28 * x * H - 12 * H2)
+                - 15 * x4
+              );
+
+  const real_type c = invH / (2 * H2);
+
+  base_D[4] = c * (2 * H - 5 * x) * dx2 * x;
+  base_D[5] = c * (3 * H - 5 * x) * x2 * dx;
+}
+
+inline void Hermite5_DD(real_type x, real_type H, real_type base_DD[6])
+{
+  // Differenza
+  const real_type dx = H - x;
+
+  // Potenze di x
+  const real_type x2 = x * x;
+
+  // Potenze di H
+  const real_type H2 = H * H;
+  const real_type H4 = H2 * H2;
+
+  // Inversi
+  const real_type invH  = real_type(1) / H;
+  const real_type invH4 = real_type(1) / H4;
+
+  // Termine comune
+  const real_type common = 60 * (H - 2 * x) * x * dx * invH * invH4;
+
+  base_DD[0] = -common;
+  base_DD[1] =  common;
+
+  base_DD[2] = 12 * invH4 * dx * (5 * x - 3 * H) * x;
+
+  base_DD[3] = 12 * invH4 * x * dx * (5 * x - 2 * H);
+
+  const real_type c = invH / H2;
+
+  base_DD[4] = c * (10 * x2 + H2 - 8 * H * x) * dx;
+
+  base_DD[5] = c * (x2 * (10 * x - 12 * H) + 3 * x * H2);
+}
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+inline void Hermite5_DDD(real_type x, real_type H, real_type base_DDD[6])
+{
+  // Potenze
+  const real_type x2 = x * x;
+  const real_type H2 = H * H;
+  const real_type H4 = H2 * H2;
+
+  // Prodotti utili
+  const real_type Hx = H * x;
+
+  // Inversi
+  const real_type invH  = real_type(1) / H;
+  const real_type invH4 = real_type(1) / H4;
+
+  // Termine comune
+  const real_type common = invH * invH4 * (360 * Hx - 60 * H2 - 360 * x2);
+
+  base_DDD[0] =  common;
+  base_DDD[1] = -common;
+
+  base_DDD[2] = invH4 * (192 * Hx - 36 * H2 - 180 * x2);
+  base_DDD[3] = invH4 * (168 * Hx - 24 * H2 - 180 * x2);
+
+  const real_type c = invH / H2;
+
+  base_DDD[4] = c * (36 * Hx - 9 * H2 - 30 * x2);
+  base_DDD[5] = c * (3 * H2 - 24 * Hx + 30 * x2);
+}
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+inline void Hermite5_DDDD(real_type x, real_type H, real_type base_DDDD[6])
+{
+  // Potenze di H
+  const real_type H2 = H * H;
+  const real_type H4 = H2 * H2;
+
+  // Inversi
+  const real_type invH  = real_type(1) / H;
+  const real_type invH4 = real_type(1) / H4;
+
+  // Termine comune
+  const real_type common = invH * invH4 * (360 * H - 720 * x);
+
+  base_DDDD[0] =  common;
+  base_DDDD[1] = -common;
+
+  base_DDDD[2] = invH4 * (192 * H - 360 * x);
+  base_DDDD[3] = invH4 * (168 * H - 360 * x);
+
+  const real_type c = invH / H2;
+
+  base_DDDD[4] = c * (36 * H - 60 * x);
+  base_DDDD[5] = c * (60 * x - 24 * H);
+}
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+inline void Hermite5_DDDDD(real_type /*x*/, real_type H, real_type base_DDDDD[6])
+{
+  // Potenze di H
+  const real_type H2 = H * H;
+  const real_type H4 = H2 * H2;
+
+  // Inversi
+  const real_type invH  = real_type(1) / H;
+  const real_type invH4 = real_type(1) / H4;
+
+  // Costanti
+  const real_type c0 = 720 * invH * invH4;
+  const real_type c1 = 360 * invH4;
+  const real_type c2 = 60  * invH / H2;
+
+  base_DDDDD[0] = -c0;
+  base_DDDDD[1] =  c0;
+
+  base_DDDDD[2] = -c1;
+  base_DDDDD[3] = -c1;
+
+  base_DDDDD[4] = -c2;
+  base_DDDDD[5] =  c2;
+}
+
+  // --------------------------------------------------------------------------
+
+  inline real_type bilinear3( real_type const p[4], real_type const M[4][4], real_type const q[4] )
+  {
+    return p[0] * ( M[0][0] * q[0] + M[0][1] * q[1] + M[0][2] * q[2] + M[0][3] * q[3] ) +
+           p[1] * ( M[1][0] * q[0] + M[1][1] * q[1] + M[1][2] * q[2] + M[1][3] * q[3] ) +
+           p[2] * ( M[2][0] * q[0] + M[2][1] * q[1] + M[2][2] * q[2] + M[2][3] * q[3] ) +
+           p[3] * ( M[3][0] * q[0] + M[3][1] * q[1] + M[3][2] * q[2] + M[3][3] * q[3] );
+  }
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  inline real_type bilinear5( real_type const p[6], real_type const M[6][6], real_type const q[6] )
+  {
+    return p[0] *
+             ( M[0][0] * q[0] + M[0][1] * q[1] + M[0][2] * q[2] + M[0][3] * q[3] + M[0][4] * q[4] + M[0][5] * q[5] ) +
+           p[1] *
+             ( M[1][0] * q[0] + M[1][1] * q[1] + M[1][2] * q[2] + M[1][3] * q[3] + M[1][4] * q[4] + M[1][5] * q[5] ) +
+           p[2] *
+             ( M[2][0] * q[0] + M[2][1] * q[1] + M[2][2] * q[2] + M[2][3] * q[3] + M[2][4] * q[4] + M[2][5] * q[5] ) +
+           p[3] *
+             ( M[3][0] * q[0] + M[3][1] * q[1] + M[3][2] * q[2] + M[3][3] * q[3] + M[3][4] * q[4] + M[3][5] * q[5] ) +
+           p[4] *
+             ( M[4][0] * q[0] + M[4][1] * q[1] + M[4][2] * q[2] + M[4][3] * q[3] + M[4][4] * q[4] + M[4][5] * q[5] ) +
+           p[5] *
+             ( M[5][0] * q[0] + M[5][1] * q[1] + M[5][2] * q[2] + M[5][3] * q[3] + M[5][4] * q[4] + M[5][5] * q[5] );
+  }
+
+  // --------------------------------------------------------------------------
+
+  /*\
+   |    _   _                     _ _       ____        _ _
+   |   | | | | ___ _ __ _ __ ___ (_) |_ ___/ ___| _ __ | (_)_ __   ___
+   |   | |_| |/ _ \ '__| '_ ` _ \| | __/ _ \___ \| '_ \| | | '_ \ / _ \
+   |   |  _  |  __/ |  | | | | | | | ||  __/___) | |_) | | | | | |  __/
+   |   |_| |_|\___|_|  |_| |_| |_|_|\__\___|____/| .__/|_|_|_| |_|\___|
+   |                                             |_|
+  \*/
 
   //! Hermite Spline Management Class
   class HermiteSpline : public CubicSplineBase
@@ -124,4 +362,4 @@ namespace Splines
 
 }  // namespace Splines
 
-// EOF: SplineHermite.hxx
+#endif  // SPLINE_HERMITE_HXX
